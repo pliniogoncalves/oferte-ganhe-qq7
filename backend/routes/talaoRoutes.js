66b4d19b356c12f58912e3170c89695657a6e705
Plinio Gonçalves <plinioricardogoncalves@gmail.com>
2024-11-04 11:35:18 -0300

    Rota de Cadastro de Talao

--- backend/routes/talaoRoutes.js
+++ backend/routes/talaoRoutes.js
@@ -1,8 +1,21 @@
 const express = require('express');
 const router = express.Router();
 const talaoController = require('../controllers/talaoController.js');
+const { insertTalao } = require('../services/talaoService.js')
 
-// Rota para a página de usuários
+// Rota para a página de Talões
 router.get('/taloes', talaoController.getUserPage);
+
+// Rota para cadastrar um novo Talão
+router.post('/taloes', async (req, res) => {
+    const {remessa, quantidade, status, estoque} = req.body;
+
+    try{
+        const newTalao = await insertTalao(remessa, quantidade, status, estoque);
+        res.status(201).json({ message: 'Talao cadastrado com sucesso!', talao: newTalao});
+    }catch(erro){
+        res.status(500).json({ message: 'Erro ao cadastrar Talão', erro: erro.message});
+    }
+});
 
 module.exports = router;
---
+++ backend/services/talaoService.js
@@ -0,0 +1,23 @@
+const pool = require('../config/database');
+
+// Função para inserir um novo Talão
+async function inserTalao(remessa, quantidade, status, estoque) {
+    const query = `
+        INSERT INTO postgres."oferte-ganhe".Usuario (remessa, quantidade_talao, status_talao, id_estoque)
+        VALUES ($1, $2, $3, 
+            (SELECT id_estoque FROM postgres."oferte-ganhe".Estoque WHERE id_loja = $4))
+        RETURNING *;
+    `;
+
+    const values = [remessa, quantidade, status, estoque];
+
+    try {
+        const result = await pool.query(query, values);
+        return result.rows[0];
+    } catch (err) {
+        console.error('Erro ao inserir usuário:', err);
+        throw err;
+    }
+}
+
+module.exports = { inserTalao };
