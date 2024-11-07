e3942ec9614d0a28b2fbd20c0201e5126a1456ef
Plinio Gonçalves <plinioricardogoncalves@gmail.com>
2024-11-07 01:43:15 -0300

    Rotas de Taloes Testadas

--- backend/routes/talaoRoutes.js
+++ backend/routes/talaoRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const talaoController = require('../controllers/talaoController.js');
-const { insertTalao } = require('../services/talaoService.js')
+const { insertTalao, searchTalao, searchTalaoByStore, editTalao, removeTalao } = require('../services/talaoService.js')
 
 //Rota de depuração para verificar se o 'api/taloes' está ativa
 router.get('/', (req, res) =>{
@@ -9,10 +9,10 @@ router.get('/', (req, res) =>{
 });
 
 // Rota para a página de Talões
-router.get('/taloes', talaoController.getUserPage);
+router.get('/taloes/page', talaoController.getUserPage);
 
 // Rota para cadastrar um novo Talão
-router.post('/taloes', async (req, res) => {
+router.post('/taloes/register', async (req, res) => {
     const {remessa, quantidade, status, estoque} = req.body;
 
     try{
@@ -22,5 +22,66 @@ router.post('/taloes', async (req, res) => {
         res.status(500).json({ message: 'Erro ao cadastrar Talão', erro: erro.message});
     }
 });
+
+// Rota para Buscar Todos os Talões
+router.get('/taloes/list', async (req, res) => {
+    try{
+        const talao = await searchTalao();
+        res.status(200).json(talao);
+    }catch(err) {
+        res.status(500).json({ message: 'Erro ao buscar todos os talões', error: err.message });
+    }
+});
+
+// Rota para Buscar um Talao pela loja
+router.get('/taloes/list/:loja', async (req, res) => {
+    const { loja } = req.params;
+
+    try {
+        const result = await searchTalaoByStore(loja);
+        if(result){
+            res.status(200).json(result);
+        }else{
+            res.status(404).json({ message: 'Talao não encontrado' });
+        }
+    }catch(err) {
+        res.status(500).json({ message: 'Erro ao buscar Talão', error: err.message });
+    }
+});
+
+// Rota para editar um Talao existente
+router.put('/taloes/edit/:loja', async (req, res) =>{
+    const { loja } = req.params;
+    const { remessa, quantidade, status } = req.body;
+
+    try{
+        const updateTalao = await editTalao(remessa, quantidade, status, loja);
+        if (updateTalao) {
+            res.status(200).json({ message: 'Talao atualizado com sucesso!', talao: updateTalao });
+        } else {
+            res.status(404).json({ message: 'Talao não encontrado.' });
+        }
+    } catch (erro) {
+        res.status(500).json({ message: 'Erro ao editar Talao', error: erro.message });
+    }
+
+});
+
+// Rota para excluir um estoque
+router.delete('/taloes/delete/:loja', async (req, res) => {
+    const { loja } = req.params;
+
+    try{
+        const removedTalao = await removeTalao(loja);
+        if(removedTalao){
+            res.status(200).json({ message: 'Talao excluído com sucesso!', talao: removedTalao });
+        }else{
+            res.status(404).json({ message: 'Talao não encontrado.' });
+        }
+    }catch(erro){
+        res.status(500).json({ message: 'Erro ao excluir Talao', error: erro.message });
+    }
+});
+
 
 module.exports = router;
--- backend/services/talaoService.js
+++ backend/services/talaoService.js
@@ -1,16 +1,16 @@
 const pool = require('../config/database');
 
 // Função para inserir um novo Talão
-async function insertTalao(remessa, quantidade, status, estoque) {
+async function insertTalao(remessa, quantidade, status, Estoque) {
     const query = `
-        INSERT INTO postgres."oferte-ganhe".Talao (remessa, quantidade_talao, status_talao, id_estoque)
+        INSERT INTO postgres."oferte-ganhe".Talao (remessa, quantidade_talao, status_talao, id_Estoque)
         VALUES ($1, $2, $3, 
-            (SELECT id_estoque FROM postgres."oferte-ganhe".Estoque WHERE id_loja = $4)
+            (SELECT id_Estoque FROM postgres."oferte-ganhe".Estoque WHERE id_loja = $4)
         )
         RETURNING *;
     `;
 
-    const values = [remessa, quantidade, status, estoque];
+    const values = [remessa, quantidade, status, Estoque];
 
     try {
         const result = await pool.query(query, values);
@@ -21,4 +21,94 @@ async function insertTalao(remessa, quantidade, status, estoque) {
     }
 }
 
-module.exports = { insertTalao };
+//Função para consultar todos os Talões
+async function searchTalao() {
+    const query =`
+        SELECT 
+            Talao.remessa, 
+            Talao.quantidade_talao, 
+            Talao.status_talao,  
+            Loja.numero_loja
+        FROM postgres."oferte-ganhe".Talao
+        JOIN postgres."oferte-ganhe".Estoque ON Talao.id_estoque = Estoque.id_estoque
+        JOIN postgres."oferte-ganhe".Loja ON Estoque.id_loja = Loja.id_loja;
+    `;
+
+    try{
+        const result = await pool.query(query);
+        return result.rows;
+    }catch(err){
+        console.error('Erro ao consultar Talao:', err);
+    }
+}
+
+//Função para buscar Talao por loja
+async function searchTalaoByStore(loja) {
+    const query = `
+        SELECT 
+            Talao.remessa, 
+            Talao.quantidade_talao, 
+            Talao.status_talao,  
+            Loja.numero_loja
+        FROM postgres."oferte-ganhe".Talao
+        JOIN postgres."oferte-ganhe".Estoque ON Talao.id_estoque = Estoque.id_estoque
+        JOIN postgres."oferte-ganhe".Loja ON Estoque.id_loja = Loja.id_loja
+        WHERE Loja.numero_loja = $1::varchar;
+    `;
+
+    const values = [loja];
+
+    try {
+        const result = await pool.query(query, values);
+        return result.rows[0];
+    } catch (err) {
+        console.error('Erro ao buscar Talao por Loja:', err);
+        throw err;
+    }
+}
+
+//Função para editar um Talao
+async function editTalao(remessa, quantidade, status, loja) {
+    const query = `
+        UPDATE postgres."oferte-ganhe".Talao
+        SET remessa = $1, quantidade_talao = $2, status_talao = $3
+        WHERE id_estoque = (
+            SELECT id_estoque FROM postgres."oferte-ganhe".Estoque 
+            WHERE id_loja = (SELECT id_loja FROM postgres."oferte-ganhe".Loja WHERE numero_loja = $4::varchar)
+        )
+        RETURNING *;
+    `;
+
+    const values = [remessa, quantidade, status, loja];
+
+    try {
+        const result = await pool.query(query, values);
+        return result.rows[0];
+    }catch(err){
+        console.error('Erro ao editar Talao:', err);
+        throw err;
+    }
+}
+
+//Função para excluir um Talao
+async function removeTalao(loja) {
+    const query = `
+        DELETE FROM postgres."oferte-ganhe".Talao
+        WHERE id_estoque = (
+            SELECT id_estoque 
+            FROM postgres."oferte-ganhe".Estoque 
+            WHERE id_loja = (SELECT id_loja FROM postgres."oferte-ganhe".Loja WHERE numero_loja = $1::varchar)
+        )
+        RETURNING *;
+    `;
+
+    try{
+        const result = await pool.query(query, [loja]);
+        return result.rows[0]; 
+    }catch (err){
+        console.error('Erro ao deletar Talao:', err);
+        throw err;
+    }
+}
+
+module.exports = { insertTalao, searchTalao, searchTalaoByStore, editTalao, removeTalao };
