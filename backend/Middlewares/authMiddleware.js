28ea16f1c9b402518b5b841f48f08fb21021bf92
Plinio Gonçalves <plinioricardogoncalves@gmail.com>
2024-11-20 22:33:54 -0300

    ajustando arquivos de proteção de rotas

--- backend/Middlewares/authMiddleware.js
+++ backend/Middlewares/authMiddleware.js
@@ -3,15 +3,15 @@ const jwt = require('jsonwebtoken');
 function authenticateToken(req, res, next) {
     const token = req.header('Authorization')?.split(' ')[1];
 
-    if (!token) {
+    if(!token){
         return res.status(401).json({ message: 'Access Denied. No token provided.' });
     }
 
-    try {
+    try{
         const verified = jwt.verify(token, process.env.JWT_SECRET);
         req.user = verified; 
         next();
-    } catch (err) {
+    }catch (err){
         res.status(403).json({ message: 'Invalid token.' });
     }
 }
--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -4,12 +4,12 @@ const pool = require('../config/database');
 async function login(req, res) {
     const { email, password } = req.body;
 
-    try {
+    try{
         // Search for the user by email
         const query = `SELECT * FROM postgres."oferte-ganhe".Users WHERE email_users = $1;`;
         const result = await pool.query(query, [email]);
 
-        if (result.rows.length === 0) {
+        if(result.rows.length === 0) {
             return res.status(404).json({ message: 'User not found.' });
         }
 
@@ -18,7 +18,7 @@ async function login(req, res) {
         // Check password
         const isPasswordValid = await authService.verifyPassword(password, user.password_users);
 
-        if (!isPasswordValid) {
+        if(!isPasswordValid) {
             return res.status(401).json({ message: 'Invalid credentials.' });
         }
 
@@ -26,7 +26,7 @@ async function login(req, res) {
         const token = authService.generateToken(user);
 
         res.status(200).json({ message: 'Login successful.', token });
-    } catch (err) {
+    }catch(err){
         console.error('Login error:', err);
         res.status(500).json({ message: 'Internal server error.', error: err.message });
     }
