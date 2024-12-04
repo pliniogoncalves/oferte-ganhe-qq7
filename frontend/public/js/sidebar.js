eff4a1221c74efe13cbc2862699328b6b0e8b3c6
Plinio Gonçalves <plinioricardogoncalves@gmail.com>
2024-12-04 10:11:52 -0300

    ajustes no sidebar

--- frontend/public/js/sidebar.js
+++ frontend/public/js/sidebar.js
@@ -9,9 +9,21 @@ document.querySelectorAll('.load-content').forEach(button => {
 
 function toggleSidebar() {
   const sidebar = document.getElementById("sidebar");
+  const overlay = document.getElementById("overlay");
   sidebar.classList.toggle("active");
+  overlay.classList.toggle("active");
 }
 
 function closeSidebar() {
   document.getElementById("sidebar").classList.remove("active");
+  document.getElementById("overlay").classList.remove("active");
 }
+
+document.addEventListener('click', function(event) {
+  const sidebar = document.getElementById("sidebar");
+  const hamburger = document.querySelector('.hamburger');
+
+  if(!sidebar.contains(event.target) && !hamburger.contains(event.target)){
+    closeSidebar();
+  }
+});
