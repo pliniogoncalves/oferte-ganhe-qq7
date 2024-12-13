371217f6f3a97ee8a4da305a7938a766baf80aba
Plinio Gonçalves <plinioricardogoncalves@gmail.com>
2024-12-13 11:55:24 -0300

    login

--- backend/middlewares/authorizePermission.js
+++ backend/middlewares/authorizePermission.js
@@ -1,16 +1,20 @@
 function authorizePermission(requiredPermission) {
-    return(req, res, next) => {
-        const userPermissions = req.user?.permissions;
+    return (req, res, next) => {
+        const user = req.user;
 
-        if(!userPermissions || !userPermissions.includes(requiredPermission)){
-            const message = 'Usuário não autorizado.';
-
-            if(req.headers['accept'] === 'application/json'){
-                return res.status(403).json({ message });
-            }
-
-            req.addMessage(message);
+        // Verifica se o usuário está autenticado
+        if (!user) {
+            req.addMessage('Usuário não autenticado.');
             return res.redirect('/login');
+        }
+
+        const userPermissions = user.permissions;
+
+        // Verifica se o usuário possui a permissão necessária
+        if (!userPermissions || !userPermissions.includes(requiredPermission)) {
+            // Adiciona uma mensagem de erro para exibição no frontend
+            req.addMessage('Você não tem permissão.');
+            return res.redirect(req.headers.referer || '/index'); // Retorna para a página anterior ou inicial
         }
 
         next();
